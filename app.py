7aedff331fe416670bc462b81fba21f62a0e5d64
trmekomed <trm.ekomed@gmail.com>
2025-03-29 06:13:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -105,10 +105,12 @@ def main():
                 col1, col2 = st.columns(2)
                 
                 with col1:
-                    # Horizontal Bar Chart for Top 10 Narasumbers
+                    # Horizontal Bar Chart for Top 10 Narasumbers, sorted in descending order
+                    sorted_narasumber_counts = narasumber_total_counts.sort_values(ascending=False).head(10)
+    
                     fig_bar = px.bar(
-                        x=narasumber_total_counts.head(10).values, 
-                        y=narasumber_total_counts.head(10).index,
+                        x=sorted_narasumber_counts.values, 
+                        y=sorted_narasumber_counts.index,
                         orientation='h',
                         title="Top 10 Narasumber",
                         labels={'x': 'Frekuensi', 'y': 'Narasumber'}
